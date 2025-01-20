ab302ae761b2050dc3abc9ca1e15a3a86493eeb8
KenTubio <tubiokenneth40@gmail.com>
2025-01-20 16:15:49 +0800

    test first section

--- src/components/first-section.jsx
+++ src/components/first-section.jsx
@@ -8,7 +8,9 @@ export default function FirstSection(){
             <header className=" flex justify-between items-center fixed left-0 right-0 top-0 px-10">
                 <img className="w-32" src="/new-logoni-bro.png" alt="" />
 
-                <button className="text-white text-4xl"><FaRegEnvelope/></button>
+                <button className="text-white text-4xl">
+                    <FaRegEnvelope/>
+                </button>
             </header>
         </Fragment>
     )
