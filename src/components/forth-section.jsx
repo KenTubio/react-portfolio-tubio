e24b7075454c2c020a5052f755a41f02dc940eb3
KenTubio <tubiokenneth40@gmail.com>
2025-02-24 09:33:00 +0800

    update projects

--- src/components/forth-section.jsx
+++ src/components/forth-section.jsx
@@ -18,7 +18,7 @@ export default function ForthSection() {
 
         {/* first mini project */}
         <div className="grid grid-cols-2 items-start gap-10 antialiased gap-y-24  lg:grid-cols-1">
-          <div className=" flex flex-col gap-7" data-aos="fade-right"
+          <div className=" flex flex-col gap-7" data-aos="fade-up"
             data-aos-duration="1500">
             <h1 className="relative font-handjet font-bold text-4xl md:text-2xl tracking-wide text-slate-300 w-full flex justify-between items-center">
               EE - Landing Page
@@ -51,7 +51,7 @@ export default function ForthSection() {
           <div
             className="w-full h-full"
 
-            data-aos="fade-left"
+            data-aos="fade-up"
             data-aos-duration="1500"
             data-aos-delay="400"
           >
@@ -68,7 +68,7 @@ export default function ForthSection() {
 
         {/* seconnd mini project */}
 
-        <div className=" flex flex-col gap-7" data-aos="fade-right"
+        <div className=" flex flex-col gap-7" data-aos="fade-up"
             data-aos-duration="1500">
             <h1 className="relative font-handjet font-bold text-4xl tracking-wide text-slate-300 w-full flex justify-between items-center md:text-2xl">
               SUPHA - E Supermarket
@@ -95,7 +95,7 @@ export default function ForthSection() {
           <div
             className="w-full h-full"
 
-            data-aos="fade-left"
+            data-aos="fade-up"
             data-aos-duration="1500"
             data-aos-delay="400"
           >
@@ -113,7 +113,7 @@ export default function ForthSection() {
 
           {/* third mini project */}
 
-        <div className=" flex flex-col gap-7" data-aos="fade-right"
+        <div className=" flex flex-col gap-7" data-aos="fade-up"
             data-aos-duration="1500">
             <h1 className="relative font-handjet font-bold text-4xl tracking-wide text-slate-300 w-full flex justify-between items-center md:text-2xl">
               CryptoWatch
@@ -140,7 +140,7 @@ export default function ForthSection() {
           <div
             className="w-full h-full"
 
-            data-aos="fade-left"
+            data-aos="fade-up"
             data-aos-duration="1500"
             data-aos-delay="400"
           >
